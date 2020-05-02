742a915d2be7ec5c6e9fcf8976afae3664f0ae4c
xshequ <59844307+xShequ@users.noreply.github.com>
2020-05-02 20:52:45 +0200

    Update GuiHelper.java
    Added code comments and cleaned code.

--- GuiHelper.java
+++ GuiHelper.java
@@ -8,19 +8,40 @@ import net.minecraft.util.ResourceLocation;
 public class GuiHelper {
 
     private static Minecraft mc = Minecraft.getMinecraft();
-
-    public void drawPicture(int x, int y, int width, int height, String location) {
+    
+    /**
+     * Renders a picture anywhere on the screen.
+     *
+     * @param x | Specify the x position of the image.
+     * @param y | Specify the y position of the image.
+     * @param width | Specify the width of the image.
+     * @param height | Specify the height of the image.
+     * @param location | Specify the location of the image.
+     * 
+     * @return nothing | Can't return anything because its a void method.
+     *
+     */
+    public void drawPicture(int x, int y, int width, int height, ResourceLocation location) {
         ResourceLocation loc = new ResourceLocation(location);
         mc.getTextureManager().bindTexture(loc);
         Gui.drawModalRectWithCustomSizedTexture(x, y, 0, 0, width, height, width, height);
     }
 
-    public void drawBackgroundPicture(int width, int height, String location) {
+    /**
+     * Used to draw a background picture on your screen.
+     * 
+     * @param width | Used tp specify the height of the image. ( Normally #getScaledWidth() )
+     * @param height | Used tp specify the height of the image. ( Normally #getScaledHeight() )
+     * @param location | Used to specify the location of the image.
+     *                 
+     * @return nothing | Can't return anything because its a void method.
+     *                 
+     */
+    public void drawBackgroundPicture(int width, int height, ResourceLocation location) {
         ScaledResolution scaledResolution = new ScaledResolution(mc);
         ResourceLocation loc = new ResourceLocation(location);
         mc.getTextureManager().bindTexture(loc);
         Gui.drawModalRectWithCustomSizedTexture(0, 0, 0, 0, scaledResolution.getScaledWidth(), scaledResolution.getScaledHeight(), scaledResolution.getScaledWidth(), scaledResolution.getScaledHeight());
         Gui.drawRect(0, 0, width, height, 0x40000000);
     }
-
 }
